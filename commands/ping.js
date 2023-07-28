20135fcbb8874980537b5ec20c069b1a672d0108
ztoli <usenix@high5.nl>
2023-07-28 20:25:17 +0000

    making commands

---
+++ commands/ping.js
@@ -0,0 +1,4 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const token = "MTEzNDU2MTAzMTI1NjAzNTM0OA.GvMRC8.5SNg6CmNvJNVoFbHdpMLvaAYqAX479a9em5-Dc"; 
 const {Client, Intents, GatewayIntentBits} = require("discord.js");
+
+
+
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 
-client.on("ready", () =>{
+client.once("ready", () => {
     console.log("The bot is online"); //message when bot is online
 });
-client.on("message", (message) => {
-    if (message.content.substring(0, 1) === "!") {
-        message.channel.send("idiot"); //reply if message has "!" as first character
-    }
-});
+
 client.login(token);
