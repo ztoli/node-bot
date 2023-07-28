71fa0ad271658e8c008471e18d1a96d80281badc
ztoli <usenix@high5.nl>
2023-07-28 20:29:38 +0000

    final push, i swear babe

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const fs = require('node:fs');
+const path = require('node:path');
 const token = "MTEzNDU2MTAzMTI1NjAzNTM0OA.GvMRC8.5SNg6CmNvJNVoFbHdpMLvaAYqAX479a9em5-Dc"; 
-const {Client, Intents, GatewayIntentBits} = require("discord.js");
+const {Client, Collection, Events, GatewayIntentBits} = require("discord.js");
 
 
 
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
 
 
 client.once("ready", () => {
