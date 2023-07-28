df15db0fd19e37646e0f1b6a8d910fe60f5bfeb6
ztoli <usenix@high5.nl>
2023-07-28 19:56:21 +0000

    lol

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('config.json');
-
-const client = new Client({ intents : [GatewayIntentBits.Guilds] });
-
-
-client.once(Events.ClientReady, c=> {
-	console.log(`ready to do shit`);
+const token = <"YOUR_SAVED_BOT_TOKEN">; 
+const {Client, Intents} = require("discord.js");
+const client = new Client({
+    intents:[
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES 
+    ]
 });
-
+client.on("ready", () =>{
+    console.log("The bot is online"); //message when bot is online
+});
+client.on("message", (message) => {
+    if (message.content.substring(0, 1) === "!") {
+        message.channel.send("idiot"); //reply if message has "!" as first character
+    }
+});
 client.login(token);
