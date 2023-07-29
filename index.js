d02737a5193c81d45210f2a45a2012767f1e6eca
ztoli <usenix@high5.nl>
2023-07-29 12:01:55 +0000

    loser girlfriend

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const {Client, Collection, Events, GatewayIntentBits} = require("discord.js");
 
 
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
 
 client.commands = new Collection();
 
