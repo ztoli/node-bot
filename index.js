a132d961cd4fcb0ea8d3d3198c30c9ac43be4a54
elo <elodie.cayez@gmail.com>
2023-07-29 15:41:16 +0200

    fix some eslint shit

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { token }  = require('./config.json'); 
-const { Player } = require("discord-player");
-const {Client, Collection, Events, GatewayIntentBits} = require("discord.js");
-
-
+const { token } = require('./config.json');
+const { Player } = require('discord-player');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
@@ -15,49 +13,53 @@ const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        // Set a new item in the Collection with the key as the command name and the value as the exported module
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
-        } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-        }
-    }
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
 }
 
 client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
+	if (!interaction.isChatInputCommand()) return;
 
-    const command = interaction.client.commands.get(interaction.commandName);
-   
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
-    }
+	const command = interaction.client.commands.get(interaction.commandName);
 
-    try {
-        await command.execute({ client: interaction.client, interaction });
-    } catch (error) {
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: false });
-        } else {
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: false });
-        }
-    }
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute({ client: interaction.client, interaction });
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({
+				content: 'There was an error while executing this command!',
+				ephemeral: false,
+			});
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: false });
+		}
+	}
 });
 
-const player = new Player(client)
+const player = new Player(client);
 
 player.extractors.loadDefault();
 
 
-client.once("ready", () => {
-    console.log("The bot is online"); //message when bot is online
+client.once('ready', () => {
+	// message when bot is online
+	console.log('The bot is online');
 });
 
 client.login(token);
