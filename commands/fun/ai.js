2ac4770a704ecdcda193bd173713e951075d3efa
Ztoli <usenix@high5.nl>
2023-07-30 21:16:04 +0200

    added stuff and AI kind of works

--- commands/fun/ai.js
+++ commands/fun/ai.js
@@ -1,13 +1,41 @@
-//const { SlashCommandBuilder } = require('discord.js');
-//const { ask } = require('/home/ubuntu/node-bot/ai.js');
-//const { answer } = await ask(prompt);
+const {SlashCommandBuilder, EmbedBuilder} = require('discord.js');
+const {Configuration, OpenAIApi} = require('openai');
 
-//module.exports = {
-//	data: new SlashCommandBuilder()
-//		.setName('ai')
-//		.setDescription('call the OpenAI API and ask away!'),
-//	const prompt  = message.content.substring{1}
-//	async execute(ask) {
-//		await interaction.reply(answer);
-//		##},
-//};
+const configuration = new Configuration({
+	apiKey: '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'
+});
+const openai = new OpenAIApi(configuration);
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('chatgpt')
+		.setDescription('talk with chatgpt!!')
+		.addStringOption(option => option.setName('question').setDescription(`This is the question for chatgpt`).setRequired(true))
+		.setDMPermission(false),
+	async execute(interaction) {
+		await interaction.deferReply();
+
+		const question = interaction.options.getString('question');
+
+		try {
+			const res = await openai.createCompletion(
+				{
+					model: "gpt-4",
+					temperature: 0.3,
+					prompt: question,
+					max_tokens : 1024,
+				})
+
+			const embed = new EmbedBuilder()
+				.setColor('Blue')
+				.setDescription(`\`\`\`${res.data.choices[0].text}\`\`\``)
+
+			await interaction.editReply({embeds: [embed]});
+		} catch (e) {
+			return await interaction.editReply({content: `Request failed with status code **${e.response.status}**`})
+		}
+
+	}
+}
+
+
