98090f43500507d2d82479945ad023f0d81c0dca
Ztoli <usenix@high5.nl>
2023-08-02 22:17:33 +0200

    Add website-screenshot thing because it looked funny

---
+++ commands/utils/webss.js
@@ -0,0 +1,40 @@
+const {SlashCommandBuilder, AttachmentBuilder, EmbedBuilder} = require('discord.js');
+const puppeteer = require('puppeteer');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('web-screenshot')
+		.setDescription('Take a screenshot of a website')
+		.addStringOption(option => option.setName('website').setDescription('the site to take a screenshot of')),
+	async execute(interaction) {
+
+		await interaction.deferReply()
+
+		const {options} = interaction;
+
+		const website = options.getString('website');
+
+		try {
+			const browser = await puppeteer.launch();
+			const page = await browser.newPage();
+			await page.goto(website);
+			await page.setViewport({width: 1920, height: 1080});
+
+			const screenshot = await page.screenshot();
+
+			await browser.close();
+
+			const buffer = Buffer.from(screenshot, 'base64');
+			const attachment = new AttachmentBuilder(buffer, {name: 'image.png'});
+
+			const embed = new EmbedBuilder()
+				.setColor("Pink")
+				.setImage('attachment://image.png')
+
+			await interaction.editReply({embeds: [embed], files: [attachment]});
+		} catch (e) {
+			await interaction.editReply({content: `Error getting a screenshot.  Try again with a valid site.`})
+		}
+	}
+
+}
