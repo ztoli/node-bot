b784e559464e11e354a7086d9c911d7f9f219a43
elo <elodie.cayez@gmail.com>
2023-08-03 18:37:37 +0200

    Hmmmm it works but it doesn't

--- commands/utils/status.js
+++ commands/utils/status.js
@@ -1,33 +1,30 @@
-const {SlashCommandBuilder, EmbedBuilder} = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('status')
 		.setDescription('sets the bot status')
 		.addStringOption(option => option.setName('status').setDescription('Status of bots presence.').setRequired(true))
-		.addStringOption(option => option.setName('type').setDescription("type of status").addChoices({
+		.addStringOption(option => option.setName('type').setDescription('type of status').addChoices({
 			name: 'playing',
-			value: `${4}`
+			value: `${4}`,
 		})),
-	async execute(interaction, client) {
-		const {options} = interaction;
+	async execute(interaction) {
+		const { options } = interaction;
 		const status = options.getString('status');
 		const type = options.getString('type');
 
-		client.user.setActivity({
+		interaction.client.user.setActivity({
 			name: status,
 			type: type - 1,
-			url: `https://bsdnix.nl/`
-		})
+			url: `https://bsdnix.nl/`,
+		});
 		const embed = new EmbedBuilder()
 			.setColor('DarkVividPink')
-			.setDescription("Bot now has the correct status.")
+			.setDescription('Bot now has the correct status.');
 
-		await interaction.reply({embeds: [embed]})
-
-
-
-	}
-}
+		await interaction.reply({ embeds: [embed] });
 
 
+	},
+};
