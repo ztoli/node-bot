fb4cacf6910a6b2ea564cbc3a18d0f83d67bbbb9
Ztoli <usenix@high5.nl>
2023-08-03 18:07:54 +0200

    added status.js

--- commands/utils/status.js
+++ commands/utils/status.js
@@ -1,22 +1,33 @@
-const {SlashCommandbBuilder, EmbedBuilder} = require('discord.js');
+const {SlashCommandBuilder, EmbedBuilder} = require('discord.js');
 
-module.exports={
-	data: new SlashCommandbBuilder()
+module.exports = {
+	data: new SlashCommandBuilder()
 		.setName('status')
 		.setDescription('sets the bot status')
 		.addStringOption(option => optoin.setName('status').setDescription('Status of bots presence.').setRequired(true))
-		.addStringOption(option => optoin.setName('type').setDescription("type of status").addChoices({ name: 'playing', value : `${4}`})),
-	async execute (interaction, client) {
+		.addStringOption(option => option.setName('type').setDescription("type of status").addChoices({
+			name: 'playing',
+			value: `${4}`
+		})),
+	async execute(interaction, client) {
 		const {options} = interaction;
 		const status = options.getString('status');
 		const type = options.getString('type');
 
 		client.user.setActivity({
-			name:
-
-
-
-			// add some embedbuilder bullshit, idk how to make it communicate with the player..
+			name: status,
+			type: type - 1,
+			url: ``
 		})
+		const embed = new EmbedBuilder()
+			.setColor('DarkVividPink')
+			.setDescription("Bot now has the correct status.")
+
+		await interaction.reply({embeds: [embed]})
+
+
+		// add some embedbuilder bullshit, idk how to make it communicate with the player..
 	}
 }
+
+
