5ddbf54c85fd2ad78e6ddcc887696799f8bec006
Ztoli <usenix@high5.nl>
2023-09-20 20:59:30 +0200

    add random image generation

---
+++ commands/images/imagebuilder.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder, EmbedBuilder } require = ('discord.js');
+const axios = require('axios');
+
+module.exports = {
+    .setname('random-image')
+    .setDescription('Get a random image :D'),
+    async execute (interaction) {
+        await interaction.deferReply({ ephemeral: true });
+
+        const image = await axios.get(`https://picsum.photos/1920/1080?random=1`)
+        const embed = new EmbedBuilder()
+        .setcolor("Blurple")
+        .setImage(`https://fastly.picsum.photos/${image.request.path}`);
+
+        await interaction.editReply({embeds:[embed]});
+    }
+}
